bf18180f723507d42c8c3a0739940bf71aa2b52f
sergeymaguire <sergeymaguire@gmail.com>
2018-10-04 09:28:43 -0700

    project

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,48 @@
-$(document).ready(function () {
+//Step 1. geocoder is loaded in initMap function, to be used later to get the zip or nearest zip code of the current user
+//Step 2. gets the current lat and long with the built in navigator(the browser object) function
+//Step 3. get the first zip code found in the nearby locations of the current user
+//Put on global level to be used later in function(Get zip code)
+var geocoder, lat, long, zip; //these are set and used
+//This is called from the index.html script link
+function initMap() {
+  geocoder = new google.maps.Geocoder();// sets geocoder
+}
+//Step 2. gets the current lat and long with the built in navigator(the browser object) function
+navigator.geolocation.getCurrentPosition(function(position) {
+  console.log(position);
+  lat = position.coords.latitude; // sets global lat variable used in getzipcode function
+  lng = position.coords.longitude; //sets global long variable used in getzipcode function
+  getZipCode(lat, lng);
+});
+
+function getZipCode(lat, long) {
+  var latlng = {
+    lat: lat,
+    lng: long
+  };
+  geocoder.geocode(  //uses the global geocoder variable object
+    {
+      location: latlng
+    },
+    function(results, status) {
+      if (status === "OK") {
+        if (results[0]) {
+          for (j = 0; j < results[0].address_components.length; j++) {
+            if (results[0].address_components[j].types[0] == "postal_code") {
+              zip = results[0].address_components[j].short_name;
+              return;
+            }
+          }
+        }
+      }
+    }
+  );
+}
+
+$(document).ready(function() {
+  $("#find-movie").on("click", getRestaurantsFromYelp); //going to call getfood function on the click event
+  $("#find-movie").on("click", movieSearch );//Going to do the movie search when you click
+
   var config = {
     apiKey: "AIzaSyBXh_OSGPOTI4ZxdxcJL5dcV3oByDTTVwc",
     authDomain: "andchill-eb480.firebaseapp.com",
@@ -10,7 +54,7 @@ $(document).ready(function () {
   firebase.initializeApp(config);
   var database = firebase.database();
 
-  $("#find-movie").on("click", function (event) {
+  function movieSearch(event) {
     var movieSearch = $("#movie-input")
       .val()
       .trim();
@@ -22,7 +66,7 @@ $(document).ready(function () {
       "https://utelly-tv-shows-and-movies-availability-v1.p.mashape.com/lookup?";
     var queryString = queryURL + "country=" + "us" + "&term=" + movieSearch;
     getMovies(queryString);
-  });
+  };
 
   function getMovies(queryURL) {
     console.log(queryURL);
@@ -33,18 +77,18 @@ $(document).ready(function () {
         "X-Mashape-Key": "dEFISQiTvwmshEJAWU2KwJRqazW0p1I0lb0jsn5LUpy7owpPJ6",
         Accept: "application/json"
       }
-    }).success(function (response) {
+    }).success(function(response) {
       console.log(response);
       $("#movies").empty();
       if ($.isArray(response.results) && response.results.length) {
-        var html = buildHtml(response.results);
+        var html = buildMovieHtml(response.results);
         $("#movies").append(html);
         addMoviesToFirebase(response.results);
       }
     });
   }
 
-  function buildHtml(results) {
+  function buildMovieHtml(results) {
     var html = " <div class='row text-center text-lg-left'>";
 
     for (var i = 0; i < results.length; i++) {
@@ -94,66 +138,49 @@ $(document).ready(function () {
       database.ref().push({
         name: results[i].name,
         FindOn: results[i].locations[0].display_name,
-        Image: results[i].picture,
+        Image: results[i].picture
       });
     }
   }
 });
 
-var geocoder,
-  lat,
-  long,
-  zip;
-navigator.geolocation.getCurrentPosition(
-  function (position) {
-    console.log(position);
-    lat = position.coords.latitude;
-    lng = position.coords.longitude;
-    getZipCode(lat, lng);
-    $("#find-movie").on("click", getFood);
-  });
-
-function initMap() {
-  geocoder = new google.maps.Geocoder;
-}
-
-function getZipCode(lat, long) {
-  var latlng = {
-    lat: lat,
-    lng: long
-  };
-  geocoder.geocode({
-    'location': latlng
-  }, function (results, status) {
-    if (status === 'OK') {
-      if (results[0]) {
-        for (j = 0; j < results[0].address_components.length; j++) {
-          if (results[0].address_components[j].types[0] == 'postal_code') {
-            console.log("Zip Code: " + results[0].address_components[j].short_name);
-            zip = results[0].address_components[j].short_name;
-            return;
-          }
-        }
-      }
-    }
-  });
-}
-
-function getFood() {
-  var queryGURL = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&key=AIzaSyDB8oKKFdugWo4hKPNthhL1ouMovHmJ_Is";
+function getRestaurantsFromYelp() {
+  if (!zip) {
+    alert("No zip code");
+    return;
+  }
+  var queryGURL =
+    "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+    lat +
+    "," +
+    lng +
+    "&key=AIzaSyDB8oKKFdugWo4hKPNthhL1ouMovHmJ_Is";
   $.ajax({
     url: queryGURL,
     method: "GET"
-  }).then(function (response) {
+  }).then(function(response) {
     console.log(response.results[4].address_components[0].long_name);
     var YqueryURL = "https://dl-yelp-help.herokuapp.com/yelp?zip=" + zip;
     $.ajax({
       url: YqueryURL,
       method: "GET"
-    }).then(function (response) {
+    }).then(function(response) {
       for (var i = 0; i < response.length; i++) {
-        var html = ""
-        html = html + "<p class='name'>" + response[i].name + "</p>" + "<p class='address'>" + response[i].location.address1 + "</p>" + "<p class='phone'>" + response[i].display_phone + "</p>" + "<p class='price'>" + response[i].price + "</p>"
+        var html = "";
+        html =
+          html +
+          "<p class='name'>" +
+          response[i].name +
+          "</p>" +
+          "<p class='address'>" +
+          response[i].location.address1 +
+          "</p>" +
+          "<p class='phone'>" +
+          response[i].display_phone +
+          "</p>" +
+          "<p class='price'>" +
+          response[i].price +
+          "</p>";
         $("#restaurant").append(html);
       }
     });
